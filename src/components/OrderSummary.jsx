68239933014c2a1e05d1491cefc80a6996d5445b
yerraakhila <yerraakhila30@gmail.com>
2023-10-24 23:10:20 +0530

    money update

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -12,11 +12,11 @@ function OrderSummary() {
       </div>
       <div className="order-summary-gap">
         <h5 className="htag">Tax Estimate</h5>
-        <h5 className="htag">$5</h5>
+        <h5 className="htag">$10</h5>
       </div>
       <div className="order-summary-gap another">
         <h4 className="h4tag">Order total</h4>
-        <h4 className="h4tag">$120</h4>
+        <h4 className="h4tag">$125</h4>
       </div>
     </div>
   );
