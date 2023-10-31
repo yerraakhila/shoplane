fceb3ed4141457ce973ca3043ebc3c0660f346c0
yerraakhila <yerraakhila30@gmail.com>
2023-11-01 01:06:14 +0530

    Fix delivery strings

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,31 +1,30 @@
 import { RiDeleteBin5Line } from "react-icons/ri";
 import { useDispatch } from "react-redux";
-import { deleteFromCart,decrementFromCart } from "../redux/reducers/CartSlice";
+import { deleteFromCart, decrementFromCart } from "../redux/reducers/CartSlice";
 import { addToCart } from "../redux/reducers/CartSlice";
 import { useNavigate } from "react-router-dom";
 
 function CartItem(props) {
-  const { image, title, price,quantity,id,category} = props.data;
+  const { image, title, price, quantity, id, category } = props.data;
   let navigate = useNavigate();
   let dispatch = useDispatch();
   function handleClick(e) {
     e.preventDefault();
     navigate("/productDetailPage/" + id);
-    
   }
-  function handleDelete(){
+  function handleDelete() {
     dispatch(deleteFromCart(props.data));
   }
-  function handleIncrement(){
+  function handleIncrement() {
     dispatch(addToCart(props.data));
   }
-  function handleDecrement(){
+  function handleDecrement() {
     dispatch(decrementFromCart(props.data));
   }
   return (
-    <div className="cart-img-details" style={{backgroundColor:"#fff"}}>
+    <div className="cart-img-details" style={{ backgroundColor: "#fff" }}>
       <div className="without-del">
-        <div className="cart-img hover" >
+        <div className="cart-img hover">
           <img src={image} width={200} height={200} onClick={handleClick} />
         </div>
         <div className="details">
@@ -34,35 +33,56 @@ function CartItem(props) {
             <p>{title}</p>
           </div>
           <div>
-          <div className="buttons">
-            <button className="symbol common" onClick={handleDecrement}>-</button>
-            <button className="numberDisplay common">{quantity}</button>
-            <button className="symbol common" onClick={handleIncrement}>+</button>
+            <div className="buttons">
+              <button className="symbol common" onClick={handleDecrement}>
+                -
+              </button>
+              <button className="numberDisplay common">{quantity}</button>
+              <button className="symbol common" onClick={handleIncrement}>
+                +
+              </button>
+            </div>
+            {category === "jewelery" ? (
+              <div className="discount">
+                <h4 style={{ margin: "0px" }}>
+                  ${(price * quantity * 0.7).toFixed(2)}
+                </h4>
+                <del>
+                  <h6 style={{ margin: "0px" }} className="original-price">
+                    ${(price * quantity).toFixed(2)}
+                  </h6>
+                </del>
+                <h6
+                  style={{ margin: "0px", color: "brown", fontWeight: "400" }}
+                >
+                  (30% off)
+                </h6>
+              </div>
+            ) : (
+              <h4 style={{ margin: "0px", marginTop: "5px" }}>
+                ${price * quantity}
+              </h4>
+            )}
+
+            {category === "women's clothing" ? (
+              <p style={{ margin: "0px", color: "green" }}>
+                Express delivery in 3 days
+              </p>
+            ) : (
+              <p style={{ margin: "0px", color: "#f5429e" }}>
+                Estimated delivery on 20 Nov 2023
+              </p>
+            )}
           </div>
-          {
-            category === "jewelery" ?
-            <div className="discount">
-            <h4 style={{margin:"0px"}}>${(price*quantity*0.7).toFixed(2)}</h4>
-            <del><h6 style={{margin:"0px"}} className="original-price">${(price*quantity).toFixed(2)}</h6></del>
-            <h6 style={{margin:"0px",color:"brown",fontWeight:"400"}}>(30% off)</h6>
-            </div> :
-            <h4 style={{margin:"0px",marginTop:"5px"}}>${price*quantity}</h4>
-          }
-          
-          {category === "women's clothing" ?
-            <p style={{margin:"0px",color:"green"}} >express delivery in 2 days</p> :
-            <p style={{margin:"0px",color:"#f5429e"}} >delivery will be on 5 Nov 2023</p>
-          }
-          </div>
-          
         </div>
       </div>
-      <div >
-      <RiDeleteBin5Line size={30} 
-      className="for-delete"
-      onClick={handleDelete} />
+      <div>
+        <RiDeleteBin5Line
+          size={30}
+          className="for-delete"
+          onClick={handleDelete}
+        />
       </div>
-      
     </div>
   );
 }
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -2,18 +2,16 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addToCart, deleteFromCart} from "../redux/reducers/CartSlice";
+import { addToCart, deleteFromCart } from "../redux/reducers/CartSlice";
 import { AiFillStar } from "react-icons/ai";
-import NavAndSub from './../components/NavAndSub';
+import NavAndSub from "./../components/NavAndSub";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { currUserCartItemsList } from "../redux/reducers/CartSlice";
 import NavbarWithSubcat from "../components/NavbarWithSubcat";
 
-
-
 function ProductDetailPage() {
   const { id } = useParams();
-  console.log(id)
+  console.log(id);
   const [product, setProduct] = useState({});
   useEffect(() => {
     axios
@@ -22,70 +20,95 @@ function ProductDetailPage() {
       .catch((error) => console.log(error));
   }, [id]);
   let cartItemsList = useSelector(currUserCartItemsList);
-  console.log(cartItemsList)
+  console.log(cartItemsList);
   let isInCart = cartItemsList.some((each) => each.id === product.id);
-  console.log(isInCart)
+  console.log(isInCart);
   let dispatch = useDispatch();
   function handleCart() {
     !isInCart
       ? dispatch(addToCart(product))
       : dispatch(deleteFromCart(product));
-    }
-  
+  }
+
   if (!product) return null;
 
-  if (!product.rating ) return null;
+  if (!product.rating) return null;
 
   return (
-    <div style={{backgroundColor:"#f7f0f0",minHeight:"100vh"}}>
-    <NavbarWithSubcat/>
-    <div className="container prods centering" >
-      <div className="wrapper">
-        <div className="row">
-          <div className="col-sm-5 cust-col-sm-6">
-            <img
-              src={product.image}
-              alt=""
-              height="400px"
-              width="300px"
-              className="img-fluid"
-            />
-          </div>
-          <div className="col-sm-7 gap">
-            <div className="spacing">
-            <h4>{product.title}</h4>
-            <p>{product.description.slice(0,500)}</p>
+    <div style={{ backgroundColor: "#f7f0f0", minHeight: "100vh" }}>
+      <NavbarWithSubcat />
+      <div className="container prods centering">
+        <div className="wrapper">
+          <div className="row">
+            <div className="col-sm-5 cust-col-sm-6">
+              <img
+                src={product.image}
+                alt=""
+                height="400px"
+                width="300px"
+                className="img-fluid"
+              />
             </div>
-            
-            <div className="spacing">
-            {product.rating.rate < 2.5 &&
-            <div className="rating">
-              <AiFillStar  size={30} className={product.rating.rate > 0   ? "red" : ""} />
-              <AiFillStar  size={30} className={product.rating.rate < 2.5 && product.rating.rate >= 1.5 ? "red" : ""} />
-              <AiFillStar  size={30} className="" />
-              <AiFillStar  size={30} className="" />
-              <AiFillStar  size={30} className="" />
-              <span style={{fontSize:"20px"}}>({product.rating.count})</span>
-            </div>}
-            {product.rating.rate > 2.5 && product.rating.rate < 4  &&
-            <div className="rating">
-              <AiFillStar  size={30} className="orange" />
-              <AiFillStar  size={30} className="orange" />
-              <AiFillStar  size={30} className="orange"/>
-              <AiFillStar  size={30} className={product.rating.rate >= 3.5 ? "orange" : ""} />
-              <AiFillStar  size={30} className="" />
-              <span style={{fontSize:"20px"}}>({product.rating.count})</span>
-            </div>}
-            { product.rating.rate >= 4 &&
-            <div className="rating">
-              <AiFillStar  size={30} className="green" />
-              <AiFillStar  size={30} className="green" />
-              <AiFillStar  size={30} className="green" />
-              <AiFillStar  size={30} className="green" />
-              <AiFillStar  size={30} className={product.rating.rate >= 4.5 ? "green" : ""} />
-              <span style={{fontSize:"20px"}}>({product.rating.count})</span>
-            </div>}
-            {/* <div className="rating">
+            <div className="col-sm-7 gap">
+              <div className="spacing">
+                <h4>{product.title}</h4>
+                <p>{product.description.slice(0, 500)}</p>
+              </div>
+
+              <div className="spacing">
+                {product.rating.rate < 2.5 && (
+                  <div className="rating">
+                    <AiFillStar
+                      size={30}
+                      className={product.rating.rate > 0 ? "red" : ""}
+                    />
+                    <AiFillStar
+                      size={30}
+                      className={
+                        product.rating.rate < 2.5 && product.rating.rate >= 1.5
+                          ? "red"
+                          : ""
+                      }
+                    />
+                    <AiFillStar size={30} className="" />
+                    <AiFillStar size={30} className="" />
+                    <AiFillStar size={30} className="" />
+                    <span style={{ fontSize: "20px" }}>
+                      ({product.rating.count})
+                    </span>
+                  </div>
+                )}
+                {product.rating.rate > 2.5 && product.rating.rate < 4 && (
+                  <div className="rating">
+                    <AiFillStar size={30} className="orange" />
+                    <AiFillStar size={30} className="orange" />
+                    <AiFillStar size={30} className="orange" />
+                    <AiFillStar
+                      size={30}
+                      className={product.rating.rate >= 3.5 ? "orange" : ""}
+                    />
+                    <AiFillStar size={30} className="" />
+                    <span style={{ fontSize: "20px" }}>
+                      ({product.rating.count})
+                    </span>
+                  </div>
+                )}
+                {product.rating.rate >= 4 && (
+                  <div className="rating">
+                    <AiFillStar size={30} className="green" />
+                    <AiFillStar size={30} className="green" />
+                    <AiFillStar size={30} className="green" />
+                    <AiFillStar size={30} className="green" />
+                    <AiFillStar
+                      size={30}
+                      className={product.rating.rate >= 4.5 ? "green" : ""}
+                    />
+                    <span style={{ fontSize: "20px" }}>
+                      ({product.rating.count})
+                    </span>
+                  </div>
+                )}
+                {/* <div className="rating">
               <AiFillStar  size={30} className={product.rating.rate > 0.5 ? "checked" : ""} />
               <AiFillStar  size={30} className={product.rating.rate > 1.5 ? "checked" : ""} />
               <AiFillStar  size={30} className={product.rating.rate > 2.5 ? "checked" : ""} />
@@ -93,41 +116,62 @@ function ProductDetailPage() {
               <AiFillStar  size={30} className={product.rating.rate > 4.5 ? "grecheckeden" : ""} />
               <span style={{fontSize:"20px"}}>({product.rating.count})</span>
             </div> */}
-            {product.category === "electronics" ?
-             <h5 style={{color:"brown"}}>Size: Onesize</h5> :
-             
-             <h5 style={{color:"brown"}}>Size: S</h5>}
-              
-            
-            
-            {product.category === "jewelery" ? 
-            <div className="discount-again">
-              <h2 style={{margin:"0px",padding:"0px"}}>${(product.price*0.7).toFixed(2)}</h2>
-              <del ><h4 style={{color:"red",margin:"0px"}}>${(product.price).toFixed(2)}</h4></del>
-              <h4 style={{fontWeight:"400",color:"brown",margin:"0px"}}>(30% off)</h4>
-
-            </div>:
-            <h2 style={{margin:"0px",padding:"0px"}}>${product.price}</h2>
-}             {product.category === "women's clothing" ?
-            <p style={{margin:"0px",color:"green"}} >express delivery in 2 days</p> :
-            <p style={{margin:"0px",color:"#f5429e"}} >delivery will be on 5 Nov 2023</p>
-          }
-            
-            <button 
-              className={!isInCart ? "btn-blue-color white-color for-padding":"btn-red-color white-color for-padding"}
-              onClick={handleCart}
-            >
-              {!isInCart ? "Add to Cart" : "Remove from Cart"}
-            </button>
+                {product.category === "electronics" ? (
+                  <h5 style={{ color: "brown" }}>Size: Onesize</h5>
+                ) : (
+                  <h5 style={{ color: "brown" }}>Size: S</h5>
+                )}
+                {product.category === "jewelery" ? (
+                  <div className="discount-again">
+                    <h2 style={{ margin: "0px", padding: "0px" }}>
+                      ${(product.price * 0.7).toFixed(2)}
+                    </h2>
+                    <del>
+                      <h4 style={{ color: "red", margin: "0px" }}>
+                        ${product.price.toFixed(2)}
+                      </h4>
+                    </del>
+                    <h4
+                      style={{
+                        fontWeight: "400",
+                        color: "brown",
+                        margin: "0px",
+                      }}
+                    >
+                      (30% off)
+                    </h4>
+                  </div>
+                ) : (
+                  <h2 style={{ margin: "0px", padding: "0px" }}>
+                    ${product.price}
+                  </h2>
+                )}{" "}
+                {product.category === "women's clothing" ? (
+                  <p style={{ margin: "0px", color: "green" }}>
+                    Express delivery in 3 days
+                  </p>
+                ) : (
+                  <p style={{ margin: "0px", color: "#f5429e" }}>
+                    Estimated delivery on 20 Nov 2023
+                  </p>
+                )}
+                <button
+                  className={
+                    !isInCart
+                      ? "btn-blue-color white-color for-padding"
+                      : "btn-red-color white-color for-padding"
+                  }
+                  onClick={handleCart}
+                >
+                  {!isInCart ? "Add to Cart" : "Remove from Cart"}
+                </button>
+              </div>
             </div>
-            </div>
-         
+          </div>
         </div>
       </div>
     </div>
-    
-    </div>
-  )
+  );
 }
 
 export default ProductDetailPage;
